0966e0218be4e6b2d1ac5a53faa8a78673c38177
y1060165121 <loarek1996@hotmail.com>
2020-06-09 15:53:43 -0500

    Change aboutus page.

--- static/javascript/navbar.js
+++ static/javascript/navbar.js
@@ -10,12 +10,31 @@ $('#zhuanyelingyu').hover(function() {
 $(window).scroll(function() {
   if ($(this).scrollTop() < 10) {
     $('#logo').animate({ "height": "150px", "margin-top": "0px", "margin-left": "0px" }, { duration: 1 })
+    // $("#feature1").fadeOut(1000)
   }
   else {
     $('#logo').animate({ "height": "60px", "margin-top": "5px", "margin-left": "35px" }, { duration: 1 })
+    // $("#feature1").fadeIn(1000)
   }
 });
+new WOW().init();
 
+// var waypoint = new Waypoint({
+//   element: document.getElementById('feature1'),
+//   handler: function(direction) {
+//     console.log('Scrolled to waypoint!')
+//     $("#feature1").fadeIn(1000)
+//   }
+// })
+// var waypoint = new Waypoint({
+//   element: document.getElementById('feature1'),
+//   handler: function(direction) {
+//     console.log('Scrolled to waypoint!')
+//     console.log(direction)
+//     $('#feature1').fadeIn(1000)
+//   },
+//   offset: 200
+// })
 // $(window).scrollTop(function() {
 //   console.log('hererer')
 //   $('#logo').css({ "height": "150px", "margin-top": "0px", "margin-left": "0px" })
